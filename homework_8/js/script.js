43324622efb7a399d42aeab8647fac6d1624078a
EvgeniyGilev <euggil@yandex.ru>
2022-10-28 11:31:32 +0500

    Update script.js
    немного отрефакторил код - вывел сообщение об ошибке в отдельную переменную

--- homework_8/js/script.js
+++ homework_8/js/script.js
@@ -3,51 +3,48 @@ button.addEventListener('click', (e) => {
   const value1 = document.getElementById('value1').value;
   const value2 = document.getElementById('value2').value;
   const operator = document.getElementById('operator').value;
+  let errorMessage;
   let result;
 
   document.getElementById('result').value = '';
 
   if (value1 === '') {
     console.log('Первое число не указано');
-    document.getElementById('message-error').innerText =
-      'Первое число не указано';
+    errorMessage = 'Первое число не указано';
   } else if (value2 === '') {
     console.log('Второе число не указано');
-    document.getElementById('message-error').innerText =
-      'Второе число не указано';
+    errorMessage = 'Второе число не указано';
   } else if (operator === '') {
     console.log('Не введён знак');
-    document.getElementById('message-error').innerText = 'Не введён знак';
+    errorMessage = 'Не введён знак';
   } else if (
     typeof +value1 !== 'number' ||
     typeof +value2 !== 'number' ||
     Number.isNaN(+value1 * +value2) === true
   ) {
     console.log('Некорректный ввод чисел');
-    document.getElementById('message-error').innerText =
-      'Некорректный ввод чисел';
+    errorMessage = 'Некорректный ввод чисел';
   } else {
     switch (operator) {
       case '-':
         result = value1 - value2;
-        document.getElementById('message-error').innerText = 'Выполнено!';
+        errorMessage = 'Выполнено!';
         break;
       case '+':
         result = +value1 + +value2;
-        document.getElementById('message-error').innerText = 'Выполнено!';
+        errorMessage = 'Выполнено!';
         break;
       case '*':
         result = value1 * value2;
-        document.getElementById('message-error').innerText = 'Выполнено!';
+        errorMessage = 'Выполнено!';
         break;
       case '/':
         result = value1 / value2;
-        document.getElementById('message-error').innerText = 'Выполнено!';
+        errorMessage = 'Выполнено!';
         break;
       default:
         console.log('Программа не поддерживает такую операцию');
-        document.getElementById('message-error').innerText =
-          'Программа не поддерживает такую операцию';
+        errorMessage = 'Программа не поддерживает такую операцию';
         result = '';
         break;
     }
@@ -58,12 +55,12 @@ button.addEventListener('click', (e) => {
       Number.isNaN(result) === true
     ) {
       console.log('Операция некорректна');
-      document.getElementById('message-error').innerText =
-        'Операция некорректна';
+      errorMessage = 'Операция некорректна';
       document.getElementById('result').value = '';
     } else {
       document.getElementById('result').value = result;
       console.log(result);
     }
   }
+  document.getElementById('message-error').innerHTML = errorMessage;
 });
